b93df29343f14c7352f245a0a9384e9c09e881a4
Lucas Santana <lucas.pesanttana@gmail.com>
2022-02-06 18:20:39 -0300

    fix colision bug

--- src/Object/Rock.gd
+++ src/Object/Rock.gd
@@ -3,6 +3,6 @@ extends Area2D
 func start(pos):
 	global_position = pos
 
-func _on_Rock_body_entered():
+func _on_Rock_body_entered(body):
 	$"/root/Settings".dano = 1
 	queue_free();
--- src/Object/Water.gd
+++ src/Object/Water.gd
@@ -3,6 +3,6 @@ extends Area2D
 func start(pos):
 	global_position = pos
 
-func _on_Water_body_entered():
+func _on_Water_body_entered(body):
 	$"/root/Settings".dash = 1
 	queue_free();
